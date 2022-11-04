cbc898950ee0eaeb27d8407ee20bbef7e546b742
Yuzi-Liang <yzliang2000@gmail.com>
2022-11-04 16:18:36 -0700

    Update forwardStepwise.py

--- code/forwardStepwise.py
+++ code/forwardStepwise.py
@@ -52,9 +52,13 @@ for step in range(1, len(predictor_list)):
     sorted_list.append(listMinus(predictor_list, sorted_list)[max_index])
     R2_list.append(max_number)
 
+
+
 print(sorted_list)
 print(R2_list)
 
+print(len(sorted_list))
+print(len(R2_list))
 
 
 
